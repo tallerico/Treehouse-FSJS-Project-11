e32b83d1876496bb4791ecbeffefb3a4fc29320a
Mike Tallerico <mtallerico1@gmail.com>
2018-12-19 21:06:19 -0500

    installs mongoose

--- src/index.js
+++ src/index.js
@@ -1,25 +1,28 @@
-'use strict';
+'use strict'
 
 // load modules
-const express = require('express');
-const morgan = require('morgan');
+const express = require('express')
+const morgan = require('morgan')
+const mongoose = require('mongoose')
 
-const app = express();
+mongoose.connect('mongodb://localhost/course-api')
+
+const app = express()
 
 // set our port
-app.set('port', process.env.PORT || 5000);
+app.set('port', process.env.PORT || 5000)
 
 // morgan gives us http request logging
-app.use(morgan('dev'));
+app.use(morgan('dev'))
 
 // TODO add additional routes here
 
 // send a friendly greeting for the root route
 app.get('/', (req, res) => {
-  res.json({
-    message: 'Welcome to the Course Review API'
-  });
-});
+	res.json({
+		message: 'Welcome to the Course Review API',
+	})
+})
 
 // uncomment this route in order to test the global error handler
 // app.get('/error', function (req, res) {
@@ -28,21 +31,21 @@ app.get('/', (req, res) => {
 
 // send 404 if no other route matched
 app.use((req, res) => {
-  res.status(404).json({
-    message: 'Route Not Found'
-  })
+	res.status(404).json({
+		message: 'Route Not Found',
+	})
 })
 
 // global error handler
 app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(err.status || 500).json({
-    message: err.message,
-    error: {}
-  });
-});
+	console.error(err.stack)
+	res.status(err.status || 500).json({
+		message: err.message,
+		error: {},
+	})
+})
 
 // start listening on our port
 const server = app.listen(app.get('port'), () => {
-  console.log(`Express server is listening on port ${server.address().port}`);
-});
+	console.log(`Express server is listening on port ${server.address().port}`)
+})
